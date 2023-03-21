b43ecac19674c40aeebf263fb1a3d5e9dd201e95
Nikolay <delonartt@gmail.com>
2023-03-21 16:29:07 +0500

    поправил отступы DATABASES

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -69,12 +69,15 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE',
+                            default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
         'HOST': os.getenv('DB_HOST', default='db'),
-        'PORT': os.getenv('DB_PORT', default='5432')
+        'PORT': os.getenv('DB_PORT', default='5432'),
+    }
+}
 
     #     'ENGINE': "django.db.backends.postgresql",
     #     'NAME': "postgres",
@@ -82,8 +85,6 @@ DATABASES = {
     #     'PASSWORD': "postgres",
     #     'HOST': "db",
     #     'PORT': "5432"
-    }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
