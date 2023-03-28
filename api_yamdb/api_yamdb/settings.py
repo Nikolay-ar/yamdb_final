a11a649cad27fefb18fb8ece416c5ed210546bd5
Nikolay <delonartt@gmail.com>
2023-03-28 16:08:05 +0500

    add 'POSTGRES_PASSWORD', default='postgres'

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
                             default='django.db.backends.postgresql'),
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
         'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432'),
     }
